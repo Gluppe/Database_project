d7e56ff5968930313bf7b85b486a4defba46fcdf
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-03-18 11:52:19 +0100

    Added test file

---
+++ tests/unit/PDOTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Codeception\Test\Unit;
+
+class PDOTest extends Unit {
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+
+    /**
+     * @var \PDODemo
+     */
+    protected $pdoDemo;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+}
