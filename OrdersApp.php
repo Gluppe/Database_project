9205919384237fc1e8442afe423493d2c60366d1
Mikael Falkenberg Krog <mikaelfk@stud.ntnu.no>
2021-04-08 12:36:19 +0000

    Changed getOrders function, added comments

--- OrdersApp.php
+++ OrdersApp.php
@@ -1,9 +1,9 @@
 <?php
-require_once 'OrdersModel.php';
+require_once 'models\OrdersModel.php';
 
 $model = new OrdersModel();
 $array = $model->getOrders();
 print_r($array);
-$nice = array("hey", "pog", "Pogchamp");
-$int = $model->addOrder($nice);
-print($int);
+//$nice = array("hey", "pog", "Pogchamp");
+//$int = $model->addOrder($nice);&
+//print($int);
