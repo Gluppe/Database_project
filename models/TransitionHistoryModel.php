cadbed1a2115fb19f1f433f23861b9ee4efb80e8
magnujg <magnujg@stud.ntnu.no>
2021-04-27 12:01:02 +0200

    updated addTransaitionHistory

--- models/TransitionHistoryModel.php
+++ models/TransitionHistoryModel.php
@@ -12,17 +12,26 @@ class TransitionHistoryModel{
     /**
      * A function for adding a new transition order
      * @param int $orderNumber The order number of the order changing state
-     * @param string $stateChange The new state of the order
+     * @param string $state The new state of the order
      */
-    public function addTransitionHistory(int $orderNumber, string $stateChange) {
-        $query = 'INSERT INTO transition_history(state_change) VALUES (:state_change) UPDATE table SET date = GETDATE()
+    public function addTransitionHistory(int $orderNumber, String $state) {
+        try {
+            $this->db->beginTransaction();
+            $query1 = 'INSERT INTO transition_history(state_change) VALUES (:state_change) UPDATE table SET date = GETDATE()
                     WHERE order_number = :order_number';
 
-        $stmt = $this->db->prepare($query);
-        $stmt->bindValue("state_change", $stateChange);
-        $stmt->bindValue("order_number", $orderNumber);
-        $stmt->execute();
+            $query2 = 'SELECT `state` FROM `order` WHERE order_number = :order_number';
 
+            $previousState = $this->db->prepare($query2);
+
+            $stmt = $this->db->prepare($query1);
+            $stmt->bindValue("state_change", $previousState . " -> " . $state);
+            $stmt->bindValue("order_number", $orderNumber);
+            $stmt->execute();
+        } catch (Throwable $e){
+            $this->db->rollBack();
+            throw $e;
+        }
     }
 
 }
