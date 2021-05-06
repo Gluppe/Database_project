6cfc11409db4d107b26b2b9011afae4afd579e35
magnujg <magnujg@stud.ntnu.no>
2021-05-06 14:43:38 +0200

    added framework for a few unit test, but they are not fully functional yet

--- tests/unit/PDOTest.php
+++ tests/unit/PDOTest.php
@@ -3,6 +3,7 @@
 use Codeception\Test\Unit;
 require_once 'dbCredentials.php';
 require_once 'models/SkisModel.php';
+require_once 'models/OrdersModel.php';
 
 class PDOTest extends Unit {
     /**
@@ -23,10 +24,60 @@ class PDOTest extends Unit {
     {
     }
 
-    public function testAddSKiNewSKiType() {
+    /**
+     * A unit test for adding a new ski, make sure there are no skis of
+     * ski_type_id 3 so the seeInDatabase wont find an already existing ski
+     * @todo Make sure the database cleans up after each test
+     */
+    public function testAddSKiNewSKiByType() {
         $SkisModel = new skisModel();
-        $SkisModel->addSki(array('ski_type_id' => 2));
+        $SkisModel->addSki(array('ski_type_id' => 3));
 
-        $this->tester->seeInDatabase('ski', array('ski_type_id' => 2));
+        $this->tester->seeInDatabase('ski', array('ski_type_id' => 3));
+    }
+
+    /*
+    public function testGetOrderByOrderNumber() {
+        $OrdersModel = new OrdersModel();
+        $OrdersModel->getOrder(array('order_number' => 1));
+
+        $this->tester->assertEquals(array('order_number' => 1));
+    }
+    */
+
+    public function testGetSkiByProductionNumber() {
+        $SkisModel = new skisModel();
+
+        $res = $SkisModel->getSki('1');
+
+        $this->tester->assertIsArray($res);
+
+        if ($res[0]['production_number'] = '1') {
+            $test1 = $res[0];
+        }
+
+        $this->tester->assertEquals('1', $test1['production_number']);
+    }
+
+    public function testCancelOrderByOrderNumber() {
+        $OrdersModel = new OrdersModel();
+        $OrdersModel->cancelOrder(array('order_number' => 428, 'customer_id' => 10));
+        $res = $OrdersModel->getOrder(array('order_number' => 428, 'customer_id' => 10));
+
+        $this->tester->assertEquals('canceled', $res['state']);
+    }
+
+
+    public function testGetSkiTypesByGripSystem() {
+        $skisModel = new skisModel();
+        $res = $skisModel->getSkiTypesByGripSystem(array('wax'));
+
+        //$this->tester->assertIsArray($res);
+
+        if ($res[0]['grip_system'] = 'wax') {
+            $test1 = $res[0];
+        }
+
+        $this->tester->assertEquals('wax', $test1['grip_system']);
     }
 }
