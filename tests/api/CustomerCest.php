c1e7cae982130e6d7ab4e7304e902e759f588c88
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-05-13 22:47:38 +0200

    Added response code check to api tests

--- tests/api/CustomerCest.php
+++ tests/api/CustomerCest.php
@@ -30,6 +30,7 @@ class CustomerCest
         $data = array('skis' => array('1' => 100, '2' => 50));
         $data = json_encode($data);
         $I->sendPost('/customer/orders?customer_id=10', $data);
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::CREATED);
         $I->seeNumRecords(3, 'order', []);
         $I->seeNumRecords(2, 'order_skis', ['order_number' => '429']);
     }
@@ -37,6 +38,7 @@ class CustomerCest
     public function testCancelOrder(ApiTester $I) {
         Authorisation::setAuthorisationTokenCustomer($I);
         $I->sendDelete('/customer/orders/1?customer_id=10');
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::ACCEPTED);
         $I->seeInDatabase('order', ['state' => 'canceled']);
         $I->seeNumRecords(10,'ski', ['order_no' => null]);
     }
