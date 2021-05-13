7578ac921ee67bc69b165c33707d94dcf6af337b
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-05-13 20:43:07 +0200

    Added api tests for getting orders on the customer endpoint

--- tests/api/Authorisation.php
+++ tests/api/Authorisation.php
@@ -1,8 +1,23 @@
 <?php
 class Authorisation
 {
-    public static function setAuthorisationToken(ApiTester $I) {
-        $cookie = new Symfony\Component\BrowserKit\Cookie('auth_token', '7f38212946ddbd7aadba90192887c5538328bb77bf3756504a1e538226fa8f51');
+    public static function setAuthorisationTokenCustomer(ApiTester $I) {
+        $cookie = new Symfony\Component\BrowserKit\Cookie('auth_token', '5b2c303a-43a9-4abf-a51a-b2d109527c7d');
+        $I->getClient()->getCookieJar()->set($cookie);
+    }
+
+    public static function setAuthorisationTokenEmployee(ApiTester $I) {
+        $cookie = new Symfony\Component\BrowserKit\Cookie('auth_token', '4e114242-a7ee-42d0-9c4e-fe25e89a5321');
+        $I->getClient()->getCookieJar()->set($cookie);
+    }
+
+    public static function setAuthorisationTokenShipper(ApiTester $I) {
+        $cookie = new Symfony\Component\BrowserKit\Cookie('auth_token', '0ee1345a-2a0c-497b-8c6e-4b25507fb931');
+        $I->getClient()->getCookieJar()->set($cookie);
+    }
+
+    public static function setAuthorisationTokenPublic(ApiTester $I) {
+        $cookie = new Symfony\Component\BrowserKit\Cookie('auth_token', '8940207e-eca3-479f-8231-6c7ddf038a78');
         $I->getClient()->getCookieJar()->set($cookie);
     }
 }
--- tests/api/CustomerCest.php
+++ tests/api/CustomerCest.php
@@ -7,15 +7,21 @@ class CustomerCest
     }
 
 
-    public function getAllOrdersByCustomerId(ApiTester $I) {
-        Authorisation::setAuthorisationToken($I);
+    public function getAllOrdersByCustomerIdCustomerEndpoint(ApiTester $I) {
+        Authorisation::setAuthorisationTokenCustomer($I);
         $I->sendGet('/customer/orders?customer_id=10');
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
+        $I->seeResponseIsJson();
         $I->seeResponseMatchesJsonType([
-            'id' => 'integer',
-            'city' => 'string',
-            'county' => 'string'
+            'order_number' => 'string',
+            'total_price' => 'string',
+            'state' => 'string',
+            'shipment_number' => 'string',
+            'customer_id' => 'string',
+            'date' => 'string',
+            'skiType_quantity' => 'array',
         ]);
-
+        $I->assertEquals(2, count(json_decode($I->grabResponse())));
+        $I->seeResponseContainsJson(array('order_number' => 1, 'order_number' => 428));
     }
 }
